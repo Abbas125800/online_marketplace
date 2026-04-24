5532db4fa75c959a1ede24f9ac34210cc6925126
Abbas125800 <am.abbasm1234@gmail.com>
2026-04-24 12:52:17 +0430

    new

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-=======
-// Bootstrap CSS و JS
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
->>>>>>> 8454719049bbdf9ba424bf28dc07193b10e21702
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
